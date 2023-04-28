9ea5eab6aa846e638080c0b8862669660dff14c7
David Molinero <doktor500@users.noreply.github.com>
2023-04-28 20:55:34 +0200

    Fix layout

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -11,9 +11,11 @@ const MovieDetail = async ({ params }: { params: { movie: MovieId } }) => {
 
   return (
     <div className="grid place-items-center">
-      <h2 className="text-2xl">{movie.title}</h2>
-      <h2 className="text-lg">Release date: {movie.releaseDate.toDateString()}</h2>
-      <h2>Duration: {movie.duration} minutes</h2>
+      <div className="w-96">
+        <h2 className="text-2xl">{movie.title}</h2>
+        <h2 className="text-lg">Release date: {movie.releaseDate.toDateString()}</h2>
+        <h2>Duration: {movie.duration} minutes</h2>
+      </div>
       <Image className="my-12 w-96" src={movie.posterPath.href} alt={movie.title} width="400" height="400" />
       <p className="w-96">{movie.overview}</p>
     </div>
