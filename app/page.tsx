8651a4f913937da68cae0aeba35e5544f61835d8
David Molinero <doktor500@users.noreply.github.com>
2023-05-02 09:54:46 +0200

    Fix Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-const Home = async () => "";
+import React, { Suspense } from "react";
+import Movies from "@/app/movies/page";
+
+const Home = async () => {
+  const component: JSX.Element = await Movies();
+  return <Suspense>{component}</Suspense>;
+};
 
 export default Home;
